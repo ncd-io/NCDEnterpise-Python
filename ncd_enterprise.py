cd4a1da27f70a9dfef5cb45111b066af992cd2d6
io-Jacob <web@controlanything.com>
2019-06-19 16:31:43 -0500

    Added support for V2 Mems + s3b obj
    The s3b object targets digimesh now and has a semi-tested method of sending data to sensor.

--- ncd_enterprise.py
+++ ncd_enterprise.py
@@ -1,5 +1,10 @@
-from digi.xbee.devices import XBeeDevice
+from digi.xbee.devices import DigiMeshDevice
+from digi.xbee.devices import RemoteDigiMeshDevice
+from digi.xbee.models.address import XBee64BitAddress
+from digi.xbee.packets.base import DictKeys
+from digi.xbee.packets.common import ATCommResponsePacket
 from functools import reduce
+import time
 
 class NCDEnterprise:
     def __init__(self, serial_port, baud_rate, callback, kwargs = {}):
@@ -13,21 +18,127 @@ class NCDEnterprise:
             '127': 'sensor_data'
         }
         self.data = {}
-        self.device = XBeeDevice(serial_port, baud_rate)
+        self.device = DigiMeshDevice(serial_port, baud_rate)
         self.device.open()
         self.callback = callback
-        self.device.add_data_received_callback(self.parse)
+        self.device.add_packet_received_callback(self.parse)
+        # self.mems_buffer = {'time': False, 'data': {}}
+        self.mems_buffer = {}
 
-    def parse(self, xbee_message):
-        type = self.payload_type[str(xbee_message.data[0])]
-        if(callable(getattr(self, type))):
-            getattr(self, type)(xbee_message.data[1:])
+    def send_data_to_address(self, address, data):
+        # print('send_data_to_address')
+        # print(address)
+        # print(type(data))
+        remote_device = RemoteDigiMeshDevice(self.device, XBee64BitAddress.from_hex_string(address))
+        self.device.send_data(remote_device, data)
+
+    # def parse(self, xbee_packet):
+    #     if not isinstance(xbee_packet, ATCommResponsePacket):
+    #         data = xbee_packet.rf_data
+    #         packet_type = self.payload_type[str(data[0])]
+    #         if(callable(getattr(self, packet_type))):
+    #             getattr(self, packet_type)(data[1:], xbee_packet.x64bit_source_addr)
+    #     else:
+    #         print(xbee_packet)
+
+    def parse(self, xbee_packet):
+        if not isinstance(xbee_packet, ATCommResponsePacket):
+            data = xbee_packet.rf_data
+            packet_type = self.payload_type[str(data[0])]
+
+
+            # print(data[2])
+            # if the length is exactly 180, assume it is a mems data packet
+            if(len(data) == 180):
+                self.buffer_mems(data[1:], xbee_packet.x64bit_source_addr)
+                return
+
+            # print(xbee_message.data[6])
+            # print(xbee_message.data[7])
+            type = self.payload_type[str(data[0])]
+            # print('parse')
+            if(callable(getattr(self, packet_type))):
+                getattr(self, packet_type)(data[1:], xbee_packet.x64bit_source_addr)
+        else:
+            print(xbee_packet)
 
 
 
         # self.sensor_data(xbee_message.data[1:])
 
-    def sensor_data(self, payload):
+    def buffer_mems(self, payload, source_address):
+        source_address = str(source_address)
+        if not self.mems_buffer.get(str_source_address, False):
+            self.mems_buffer[str_source_address] = {}
+
+        if payload[1] not in self.mems_buffer.get(str_source_address):
+            self.mems_buffer[str_source_address][payload[1]] = payload[4:]
+        else:
+            print('error reported')
+
+        # print(self.mems_buffer)
+        if(len(self.mems_buffer.get(str_source_address)) == 12):
+            self.parse_mems(self.mems_buffer.get(str_source_address), str_source_address)
+            self.mems_buffer[str_source_address] = {}
+
+    # def buffer_mems(self, payload, source_address):
+    #     if self.mems_buffer['time'] < time.time()-5:
+    #         self.mems_buffer = {'time': False, 'data': {}}
+    #
+    #     if not self.mems_buffer['time']:
+    #         self.mems_buffer['time'] = time.time()
+    #
+    #     if payload[1] not in self.mems_buffer['data']:
+    #         self.mems_buffer['data'][payload[1]] = payload[4:]
+    #     else:
+    #         print('error reported')
+    #
+    #     # print(self.mems_buffer)
+    #     if(len(self.mems_buffer['data']) == 12):
+    #         self.parse_mems(self.mems_buffer['data'], source_address)
+    #         self.mems_buffer = {'time': False, 'data': {}}
+
+    def parse_mems(self, mems_dict, source_address):
+        # print(mems_dict)
+        readings = 29
+        bytes_in_single = 6
+        reading_array = {}
+        for index, packet in enumerate(mems_dict):
+            packet_data = mems_dict.get(packet)[5:]
+            packet_array = {}
+            for reading in range(1, readings+1):
+
+                if index is not 12 and reading < 22:
+                    reading_array[((index*readings)+reading)] = packet_data[((reading-1)*(bytes_in_single)):(reading-1)*(bytes_in_single)+bytes_in_single]
+
+        for sample in reading_array:
+            print('77777')
+            # print(type(bytearray(sample)))
+            sample_data = reading_array.get(sample)
+            reading_array[sample] = {
+                'rms_x': signInt(reduce(msbLsb, sample_data[0:2]), 16),
+                'rms_y': signInt(reduce(msbLsb, sample_data[2:4]), 16),
+                'rms_z': signInt(reduce(msbLsb, sample_data[4:6]), 16)
+            }
+        print(reading_array)
+        parsed = {
+            'nodeId': mems_dict[1][0],
+            'firmware': "NA",
+            'battery': "NA",
+            'battery_percent': str(((msbLsb(mems_dict[1][2], mems_dict[1][3]) * 0.00322) - 1.3)/2.03*100) + "%",
+            'counter': "NA",
+            'sensor_type_id': 40,
+            'source_address': str(source_address),
+            'sensor_type_string': "Vibration Time Series",
+            'sensor_data': reading_array
+        }
+        self.callback(parsed)
+        # return reading_array
+        print("!!!!!!!!!!!!!!!!!")
+
+
+    def sensor_data(self, payload, source_address):
+        # print(payload)
         parsed = {
             'nodeId': payload[0],
             'firmware': payload[1],
@@ -35,21 +146,26 @@ class NCDEnterprise:
             'battery_percent': str(((msbLsb(payload[2], payload[3]) * 0.00322) - 1.3)/2.03*100) + "%",
             'counter': payload[4],
             'sensor_type_id': msbLsb(payload[5], payload[6]),
+            'source_address': str(source_address),
         }
         parsed['sensor_type_string'] = self.sensor_types[str(parsed['sensor_type_id'])]['name']
         parsed['sensor_data'] = self.sensor_types[str(parsed['sensor_type_id'])]['parse'](payload[8:])
+
         self.callback(parsed)
 
-    def power_up(self, payload):
+    def power_up(self, payload, source_address):
         return {
             'nodeId': payload[0],
             'sensor_type': msbLsb(payload[2], payload[3]),
+            'source_address': str(source_address)
         }
-    def config_ack(self, payload):
+
+    def config_ack(self, payload, source_address):
         return {
             'nodeId': payload[0],
             'counter': payload[1],
-            'sensor_type': msbLsb(payload[2], payload[3])
+            'sensor_type': msbLsb(payload[2], payload[3]),
+            'source_address': str(source_address)
         }
     def stop(self):
         self.device.close()
@@ -207,6 +323,28 @@ def sensor_types():
                 'counts_2': msbLsb(d[2], d[3])
             }
         },
+        "40": {
+            'name': "Vibration",
+            'parse': lambda d : {
+                'rms_x': signInt(reduce(msbLsb, d[0:3]), 24)/100,
+                'rms_y': signInt(reduce(msbLsb, d[3:6]), 24)/100,
+                'rms_z': signInt(reduce(msbLsb, d[6:9]), 24)/100,
+                'max_x': signInt(reduce(msbLsb, d[9:12]), 24)/100,
+                'max_y': signInt(reduce(msbLsb, d[12:15]), 24)/100,
+                'max_z': signInt(reduce(msbLsb, d[15:18]), 24)/100,
+                'min_x': signInt(reduce(msbLsb, d[18:21]), 24)/100,
+                'min_y': signInt(reduce(msbLsb, d[21:24]), 24)/100,
+                'min_z': signInt(reduce(msbLsb, d[24:27]), 24)/100,
+                'temperature': msbLsb(d[27], d[28])
+            }
+        },
+        "41": {
+            'name': "Unknown",
+            'parse': lambda d :	{
+                'counts_1': msbLsb(d[0], d[1]),
+                'counts_2': msbLsb(d[2], d[3])
+            }
+        },
         # untested
 		# "10006":{
 		# 	'name': "4-Channel 4-20 mA Input",
@@ -240,6 +378,13 @@ def msbLsb(m,l):
     return (m<<8)+l
 
 def signInt(i, b):
+    print('+++')
+    print(i)
+    print(b)
+    i = int(i)
+    b = int(b)
+    print(i)
+    print(b)
     if(i < 1<<(b-1)):
         return i
     return (i - (1<<b) + 1)
